ffd3c6e1a7be6fa109bc4a41157a6076984b76b5
Prulm <jaredyared83@gmail.com>
2023-11-13 14:11:50 +0300

    language selection

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_designs_practice/screens/localization.dart';
 import 'package:flutter_designs_practice/screens/login.dart';
 
 void main() async {
@@ -22,12 +23,16 @@ class Practice extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       title: 'Flutter Practice',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginScreen(),
+      home: const LocalizationPractice(),
     );
   }
 }
--- lib/screens/localization.dart
+++ lib/screens/localization.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 class LocalizationPractice extends StatefulWidget {
@@ -12,10 +13,81 @@ class _LocalizationPracticeState extends State<LocalizationPractice> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Localizations"),
+        title: Text("Language Selection").tr(),
       ),
       body: Center(
-        
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                context.setLocale(Locale("en", "US"));
+              },
+              child: Text(
+                "English",
+                style: TextStyle(
+                  color: context.locale == Locale('en', 'US')
+                      ? Colors.white
+                      : Colors.blue,
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: context.locale == Locale('en', 'US')
+                    ? Colors.blue
+                    : Colors.white,
+              ),
+            ),
+            SizedBox(height: 50),
+            ElevatedButton(
+              onPressed: () {
+                context.setLocale(Locale("am", "ET"));
+                print(Locale('am', 'ET'));
+              },
+              child: Text(
+                "Amharic",
+                style: TextStyle(
+                  color: context.locale == Locale('am', 'ET')
+                      ? Colors.white
+                      : Colors.blue,
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor:
+                    context.locale == Locale('am', 'ET') ? Colors.blue : Colors.white,
+              ),
+            ),
+            SizedBox(
+              height: 150,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Container(
+                width: 75,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text(
+                      "Next",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ).tr(),
+                    Icon(
+                      Icons.arrow_forward,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.zero,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
